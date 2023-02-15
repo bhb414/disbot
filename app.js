d81084b5cca7c5d4fbad86cef10ebc4bc854a7a8
bhb414 <72054608+bhb414@users.noreply.github.com>
2023-02-16 02:02:16 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,723 @@
+
+const items250 = [
+    {
+        name:"흑토끼 돌격대장 용병",
+        randomValue:"0.6%",
+        num:"1"
+    },
+    {
+        name:"흑토끼 밀정 스파이",
+        randomValue:"0.6%",
+        num:"2"
+
+    },
+    {
+        name:"흑토끼 수호단장 예언자",
+        randomValue:"0.6%",
+        num:"3"
+    },
+    {
+        name:"흑토끼 테두리",
+        randomValue:"0.6%",
+        num:"4"
+    },
+    {
+        name:"산수문 나전 테두리",
+        randomValue:"0.6%",
+        num:"5"
+    },
+    {
+        name:"토끼 문양 허리패",
+        randomValue:"0.6%",
+        num:"6"
+    },
+    {
+        name:"토끼 문양 나전장",
+        randomValue:"0.6%",
+        num:"7"
+    },
+    {
+        name:"나전칠기 보석함",
+        randomValue:"0.4%",
+        num:"8"
+    },
+    {
+        name:"살수 자경단원",
+        randomValue:"0.15%",
+        num:"9"
+    },
+    {
+        name:"도사 해커",
+        randomValue:"0.15%",
+        num:"10"
+    },
+    {
+        name:"흑호랑단 테두리",
+        randomValue:"0.15%",
+        num:"11"
+    },
+    {
+        name:"기복 테두리",
+        randomValue:"0.15%",
+        num:"12"
+    },
+    {
+        name:"호랑이 가죽",
+        randomValue:"0.15%",
+        num:"13"
+    },
+    {
+        name:"신년 맞이 부적",
+        randomValue:"0.15%",
+        num:"14"
+    },
+    {
+        name:"사령술사 과학자",
+        randomValue:"0.15%",
+        num:"15"
+    },
+    {
+        name:"백이무기 정치인",
+        randomValue:"0.15%",
+        num:"16"
+    },
+    {
+        name:"주작 : 죽음의 불꽃 마피아",
+        randomValue:"0.15%",
+        num:"17"
+    },
+    {
+        name:"현무 : 강철의 수호신 군인",
+        randomValue:"0.15%",
+        num:"18"
+    },
+    {
+        name:"홍단 스파이",
+        randomValue:"0.15%",
+        num:"19"
+    },
+    {
+        name:"청단 영매",
+        randomValue:"0.15%",
+        num:"20"
+    },
+    {
+        name:"달토끼 영매",
+        randomValue:"0.15%",
+        num:"21"
+    },
+    {
+        name:"만월의 짐승인간",
+        randomValue:"0.15%",
+        num:"22"
+    },
+    {
+        name:"방아꾼 건달",
+        randomValue:"0.15%",
+        num:"23"
+    },
+    {
+        name:"충성스러운 백사",
+        randomValue:"0.15%",
+        num:"24"
+    },
+    {
+        name:"황금 복돼지의 축복",
+        randomValue:"0.15%",
+        num:"25"
+    },
+    {
+        name:"이무기의 집착",
+        randomValue:"0.15%",
+        num:"26"
+    },
+    {
+        name:"설날 테두리",
+        randomValue:"0.15%",
+        num:"27"
+    },
+    {
+        name:"요괴 지갑",
+        randomValue:"0.1%",
+        num:"28"
+    },
+    {
+        name:"화투 42",
+        randomValue:"0.15%",
+        num:"29"
+    },
+    {
+        name:"자 : 명암의 심판관 심리학자",
+        randomValue:"0.15%",
+        num:"30"
+    },
+    {
+        name:"축 : 선택받은 추종자 광신도",
+        randomValue:"0.15%",
+        num:"31"
+    },
+    {
+        name:"선계의 기둥",
+        randomValue:"0.15%",
+        num:"32"
+    },
+    {
+        name:"축신의 갑주",
+        randomValue:"0.15%",
+        num:"33"
+    },
+    {
+        name:"신목 조각 테두리",
+        randomValue:"0.15%",
+        num:"34"
+    },
+    {
+        name:"승천 테두리",
+        randomValue:"0.15%",
+        num:"35"
+    },
+    {
+        name:"십이지신의 광휘",
+        randomValue:"0.1%",
+        num:"36"
+    },
+    {
+        name:"명절선물 테러리스트",
+        randomValue:"1.5%",
+        num:"37"
+    },
+    {
+        name:"풍년농부 도굴꾼",
+        randomValue:"1.5%",
+        num:"38"
+    },
+    {
+        name:"스포이드",
+        randomValue:"4.2%",
+        num:"39"
+    },
+    {
+        name:"마법의 염색약",
+        randomValue:"4.2%",
+        num:"40"
+    },
+    {
+        name:"신분증",
+        randomValue:"4.2%",
+        num:"41"
+    },
+    {
+        name:"사망광고판",
+        randomValue:"4.2%",
+        num:"42"
+    },
+    {
+        name:"부고 기사",
+        randomValue:"4.2%",
+        num:"43"
+    },
+    {
+        name:"고대의 제작서",
+        randomValue:"4.2%",
+        num:"44"
+    },
+    {
+        name:"마법의 연마제",
+        randomValue:"4.2%",
+        num:"45"
+    },
+    {
+        name:"명장의 망치",
+        randomValue:"4.2%",
+        num:"46"
+    },
+    {
+        name:"큐피드의 화살",
+        randomValue:"4.2%",
+        num:"47"
+    },
+    {
+        name:"고급 엽서",
+        randomValue:"4.2%",
+        num:"48"
+    },
+    {
+        name:"징벌의 엽서",
+        randomValue:"4.2%",
+        num:"49"
+    },
+    {
+        name:"깜짝 엽서",
+        randomValue:"4.2%",
+        num:"50"
+    },
+    {
+        name:"확성기",
+        randomValue:"4.2%",
+        num:"51"
+    },
+    {
+        name:"2티어 카드",
+        randomValue:"4.2%",
+        num:"52"
+    },
+    {
+        name:"3티어 카드",
+        randomValue:"4.2%",
+        num:"53"
+    },
+    {
+        name:"현무의 등껍질",
+        randomValue:"6.33%",
+        num:"54"
+    },
+    {
+        name:"주작의 깃털",
+        randomValue:"6.33%",
+        num:"55"
+    },
+    {
+        name:"세뱃돈",
+        randomValue:"6.32%",
+        num:"56"
+    },
+    {
+        name:"설날 확성기",
+        randomValue:"6.32%",
+        num:"57"
+    }
+];
+
+
+
+const items750 = [
+    {
+        name:"흑토끼 돌격대장 용병",
+        randomValue:"2.6%",
+        num:"1"
+    },
+    {
+        name:"흑토끼 밀정 스파이",
+        randomValue:"2.6%",
+        num:"2"
+
+    },
+    {
+        name:"흑토끼 수호단장 예언자",
+        randomValue:"2.6%",
+        num:"3"
+    },
+    {
+        name:"흑토끼 테두리",
+        randomValue:"2.6%",
+        num:"4"
+    },
+    {
+        name:"산수문 나전 테두리",
+        randomValue:"2.6%",
+        num:"5"
+    },
+    {
+        name:"토끼 문양 허리패",
+        randomValue:"2.6%",
+        num:"6"
+    },
+    {
+        name:"토끼 문양 나전장",
+        randomValue:"2.6%",
+        num:"7"
+    },
+    {
+        name:"나전칠기 보석함",
+        randomValue:"2%",
+        num:"8"
+    },
+    {
+        name:"살수 자경단원",
+        randomValue:"1%",
+        num:"9"
+    },
+    {
+        name:"도사 해커",
+        randomValue:"1%",
+        num:"10"
+    },
+    {
+        name:"흑호랑단 테두리",
+        randomValue:"1%",
+        num:"11"
+    },
+    {
+        name:"기복 테두리",
+        randomValue:"1%",
+        num:"12"
+    },
+    {
+        name:"호랑이 가죽",
+        randomValue:"1%",
+        num:"13"
+    },
+    {
+        name:"신년 맞이 부적",
+        randomValue:"1%",
+        num:"14"
+    },
+    {
+        name:"사령술사 과학자",
+        randomValue:"1%",
+        num:"15"
+    },
+    {
+        name:"백이무기 정치인",
+        randomValue:"1%",
+        num:"16"
+    },
+    {
+        name:"주작 : 죽음의 불꽃 마피아",
+        randomValue:"1%",
+        num:"17"
+    },
+    {
+        name:"현무 : 강철의 수호신 군인",
+        randomValue:"1%",
+        num:"18"
+    },
+    {
+        name:"홍단 스파이",
+        randomValue:"1%",
+        num:"19"
+    },
+    {
+        name:"청단 영매",
+        randomValue:"1%",
+        num:"20"
+    },
+    {
+        name:"달토끼 영매",
+        randomValue:"1%",
+        num:"21"
+    },
+    {
+        name:"만월의 짐승인간",
+        randomValue:"1%",
+        num:"22"
+    },
+    {
+        name:"방아꾼 건달",
+        randomValue:"1%",
+        num:"23"
+    },
+    {
+        name:"충성스러운 백사",
+        randomValue:"1%",
+        num:"24"
+    },
+    {
+        name:"황금 복돼지의 축복",
+        randomValue:"1%",
+        num:"25"
+    },
+    {
+        name:"이무기의 집착",
+        randomValue:"1%",
+        num:"26"
+    },
+    {
+        name:"설날 테두리",
+        randomValue:"1%",
+        num:"27"
+    },
+    {
+        name:"요괴 지갑",
+        randomValue:"0.6%",
+        num:"28"
+    },
+    {
+        name:"화투 42",
+        randomValue:"1%",
+        num:"29"
+    },
+    {
+        name:"자 : 명암의 심판관 심리학자",
+        randomValue:"1%",
+        num:"30"
+    },
+    {
+        name:"축 : 선택받은 추종자 광신도",
+        randomValue:"1%",
+        num:"31"
+    },
+    {
+        name:"선계의 기둥",
+        randomValue:"1%",
+        num:"32"
+    },
+    {
+        name:"축신의 갑주",
+        randomValue:"1%",
+        num:"33"
+    },
+    {
+        name:"신목 조각 테두리",
+        randomValue:"1%",
+        num:"34"
+    },
+    {
+        name:"승천 테두리",
+        randomValue:"1%",
+        num:"35"
+    },
+    {
+        name:"십이지신의 광휘",
+        randomValue:"0.6%",
+        num:"36"
+    },
+    {
+        name:"신비한 합창",
+        randomValue:"0.6%",
+        num:"37"
+    },
+    {
+        name:"마법의 염색약",
+        randomValue:"4.3%",
+        num:"38"
+    },
+    {
+        name:"깜짝 엽서",
+        randomValue:"4.3%",
+        num:"39"
+    },
+    {
+        name:"징벌의 엽서",
+        randomValue:"4.3%",
+        num:"40"
+    },
+    {
+        name:"주사위 엽서",
+        randomValue:"4.3%",
+        num:"41"
+    },
+    {
+        name:"3티어 카드",
+        randomValue:"4.3%",
+        num:"42"
+    },
+    {
+        name:"4티어 카드",
+        randomValue:"4.3%",
+        num:"43"
+    },
+    {
+        name:"현무의 등껍질",
+        randomValue:"6.5%",
+        num:"44"
+    },
+    {
+        name:"주작의 깃털",
+        randomValue:"6.5%",
+        num:"45"
+    },
+    {
+        name:"세뱃돈",
+        randomValue:"6.6%",
+        num:"46"
+    },
+    {
+        name:"설날 확성기",
+        randomValue:"6.6%",
+        num:"47"
+    }
+];
+
+const ratio250 =[];
+const ratio750 = [];
+
+const btn250 = document.getElementById("box250");
+const btn750 = document.getElementById("box750");
+
+const startbtn = document.getElementById("start");
+
+const resetBtn = document.getElementById("reset");
+
+const tryInput = document.getElementById("try");
+const lunaInput = document.getElementById("luna");
+const moneyInput = document.getElementById("money");
+
+const canvas = document.getElementById("notice-box");
+const ctx = canvas.getContext("2d");
+
+const banner = document.getElementsByClassName("banner");
+
+const BANNER_KEY = "invert";
+
+let isBannerInvert = false;
+
+let isSelect250 = true;
+let item250;
+let item750;
+let tryCount=0;
+let lunaCount=0;
+let moneyCount=0;
+
+let totalRatio1=0;
+let totalRatio2=0;
+
+var itemRatio = items250[0].randomValue;
+
+createRatioList(250);
+createRatioList(750);
+
+function createRatioList(num){
+    if(num==250){
+        
+        for(var i = 0; i<items250.length;i++){
+            var itemRatio1 = items250[i].randomValue.split('%');
+            totalRatio1 +=Math.floor(Number(itemRatio1[0])*100);
+            ratio250.push(totalRatio1);
+        }
+
+    } else if(num==750){
+        
+        for(var o = 0; o<items750.length;o++){
+            var itemRatio2 = items750[o].randomValue.split('%');
+            totalRatio2 +=Math.floor(Number(itemRatio2[0])*100);
+            ratio750.push(totalRatio2);
+        }
+    }
+}
+
+function onBox250Select(){
+    isSelect250 = true;
+    onBoxSelect();
+}
+
+function onBox750Select(){
+    isSelect250 = false;
+    onBoxSelect();
+}
+
+function onBoxSelect(){
+    if(isSelect250){
+        btn250.classList.remove("btn-unselect");
+        btn750.classList.remove("btn-select");
+        btn250.classList.add("btn-select");
+        btn750.classList.add("btn-unselect");
+    } else{
+        btn750.classList.remove("btn-unselect");
+        btn250.classList.remove("btn-select");
+        btn750.classList.add("btn-select");
+        btn250.classList.add("btn-unselect");
+    }
+}
+
+function onStartBtnClick(){
+    if(isSelect250){
+        output250();
+
+        lunaCount+=175;
+        lunaInput.value=`${lunaCount}`;
+
+        tryCount+=1;
+        tryInput.value=`${tryCount}`;
+
+        moneyCount+=2847;
+        moneyInput.value=`${moneyCount}`;
+    } else{
+        output750();
+
+        lunaCount+=750;
+        lunaInput.value=`${lunaCount}`;
+
+        tryCount+=1;
+        tryInput.value=`${tryCount}`;
+
+        moneyCount+=12200;
+        moneyInput.value=`${moneyCount}`;
+    }
+}
+
+function output250(){
+    ctx.font = '20px godic';
+    randomItem250();
+
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.save();
+    ctx.translate(20,20);
+    ctx.fillText(`${item250.name}`,0,canvas.height/4);
+    ctx.fillText(`${item250.randomValue}`,0,canvas.height/1.5);
+    ctx.restore();
+}
+
+function output750(){
+    ctx.font = '20px godic';
+    randomItem750();
+
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.save();
+    ctx.translate(20,20);
+    ctx.fillText(`${item750.name}`,0,canvas.height/4);
+    ctx.fillText(`${item750.randomValue}`,0,canvas.height/1.5);
+    ctx.restore();
+    
+
+}
+
+function onResetBtnClick(){
+    lunaCount=0;
+    lunaInput.value=`${lunaCount}`;
+
+    tryCount=0;
+    tryInput.value=`${tryCount}`;
+
+    moneyCount=0;
+    moneyInput.value=`${moneyCount}`;
+
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+}
+
+function randomItem750(){
+    let ratio = Math.random()*10000;
+    for(var i = 0; i<ratio750.length;i++){
+        if(ratio750[i]>ratio){
+            item750 = items750[i];
+            break;
+        }
+    }
+}
+
+function randomItem250(){
+    let ratio = Math.random()*10000;
+    for(var i = 0; i<ratio250.length;i++){
+        if(ratio250[i]>ratio){
+            item250 = items250[i];
+            break;
+        }
+    }
+}
+
+function invertBanner(){
+
+    for(let i = 0; i<banner.length;i++){
+        let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        if(isBannerInvert){
+            isBannerInvert=false;
+            banner[i].style.backgroundColor=randomColor;
+            //banner[i].classList.remove(BANNER_KEY);
+            
+        } else{
+            isBannerInvert=true;
+            //banner[i].classList.add(BANNER_KEY);
+            
+        }
+    }
+}
+
+
+
+btn250.addEventListener("click",onBox250Select);
+btn750.addEventListener("click",onBox750Select);
+startbtn.addEventListener("click",onStartBtnClick);
+resetBtn.addEventListener("click",onResetBtnClick);
+
+
+setInterval(invertBanner,1500);
